767e3cecf76cd8048263c689f014f3ddc573f229
Mitchell Doughty <doughtmw12@gmail.com>
2019-11-12 11:30:18 -0500

    Initial commit.

---
+++ BoundingBoxUtilsUnity/Assets/Scripts/DrawBoundingBoxes.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBoundingBoxes : MonoBehaviour
+{
+    [System.Serializable]
+    public struct BoundingBox
+    {
+        public Vector2 topLeft;
+        public Vector2 bottomRight;
+        public string label;
+        public Color color;
+    }
+
+    public List<BoundingBox> boundingBoxes;
+    public GameObject textBoxPrefab;
+
+    private Material _material;
+    private Texture2D _texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get material component from attached game object via the mesh renderer.
+        _material = this.gameObject.GetComponent<MeshRenderer>().material;
+
+        // Create a new texture instance with same size as the canvas.
+        _texture = new Texture2D(1268, 720);
+
+        // Set the texture to transparent (with helper method)
+        _texture = Texture2DExtension.TransparentTexture(_texture);
+
+        // Draw bounding boxes at specified coordinates.
+        foreach (var boundingBox in boundingBoxes)
+        {
+            _texture = Texture2DExtension.Box(
+                _texture,
+                boundingBox.topLeft,
+                boundingBox.bottomRight,
+                boundingBox.color);
+
+            // Create a new 3D text object at position and 
+            // set the label string. Canvas is scaled to x = -0.5, 5
+            // and y = -0.5, 0.5.
+            var xText = ((boundingBox.bottomRight.x / 1268.0f) - 0.5f) - 0.18f;
+            var yText = (0.5f - (1.0f - (boundingBox.bottomRight.y / 720.0f))) - 0.1f;
+            var thisBoundingBox = Instantiate(
+                textBoxPrefab,
+                Vector3.zero,
+                Quaternion.identity,
+                this.gameObject.transform) as GameObject;
+
+            thisBoundingBox.transform.localPosition = new Vector3(xText, yText, 0f);
+
+            // Set the label of the bounding box.
+            thisBoundingBox.GetComponent<TextMesh>().text = boundingBox.label;
+            thisBoundingBox.GetComponent<TextMesh>().color = boundingBox.color;
+        }
+
+
+        // Apply and set main material texture;
+        _texture.Apply();
+        _material.mainTexture = _texture;
+
+        ScreenCapture.CaptureScreenshot("Assets/catBoundingBox.jpg");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BoundingBoxUtils-Unity
 Simple Unity project to draw bounding boxes on a scaled canvas.
+
+![Cat bounding box](https://github.com/doughtmw/BoundingBoxUtils-Unity/BoundingBoxUtilsUnity/Assets/blob/master/catBoundingBox.jpg)
+
+## Requirements
+- Tested with [Unity 2018.4 LTS](https://unity3d.com/unity/qa/lts-releases
+)
+- [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/)
+
+## Bounding Box Sample
+A simple Unity scene is included, which allows the user to specify coordinates of a bounding box: top left and bottom right corners, a label, and confidence measure of a detected object and display these on a transparent canvas. 
